229e9a2760cd01bd466b283621b4762a5b6294b1
NWScraper <nws@romanpeters.nl>
2018-05-24 14:47:48 +0200

    Error handling

--- tools/check_status.py
+++ tools/check_status.py
@@ -6,29 +6,45 @@ Script to check if sites are responding.
 import urllib.request
 import urllib.error
 import csv
+import ssl
 
 # files used:
-input_file = "input.txt"
-output_file = "output.csv"
+input_file = "urls.txt"
+output_file = "check_url.csv"
 
 with open(input_file, 'r') as f:  # open input_file
     sites = f.readlines()  # split on lines
 
 result = []
+n = 1
+print('lines:', len(sites))
 for url in sites:
     url = url.strip()
     if url:
         if '://' not in url:  # add URL-scheme if missing
             new_url = ''.join(['http://', url])
-        print(new_url, end=' ')
+        else:
+            new_url = url
+        print(n, new_url, end=' ')
+
+        req = urllib.request.Request(new_url, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        })
+        context = ssl._create_unverified_context()  # avoid/ignore SSL errors
         try:
-            req = urllib.request.urlopen(new_url)
+            res = urllib.request.urlopen(req, context=context)
+            status = res.code
+        # URLErrors seem to always get caught before HTTPErrors
+        except urllib.error.HTTPError as e:
+            status = e.code
         except urllib.error.URLError as e:
-            req.code = e
-        print(req.code)
-        if url == new_url:
-            new_url = ''
-        result.append([url, new_url, req.code])
+            status = res.reason
+        except Exception as e:
+            status = e
+
+        print(status)
+        result.append([url, new_url, status])
+        n += 1
 
 # write to CSV
 with open(output_file, 'w+') as f:  # open output_file
